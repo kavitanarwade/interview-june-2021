dcd4cc6beb2504e68d36f4be6db05401a5720630
kavitanarwade <56721871+kavitanarwade@users.noreply.github.com>
2021-06-22 14:15:11 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,20 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    int binaryNo = new int[1000];
+    int i=0;
+    
+    while (n>0)
+      binaryNo[i] = n%2;
+    n = n/2 ;
+    i++;
   }
+  String res = "";
+  for(int j = i-1 ;j >= 0; j--)
+    int x=binaryNo[j];
+  res += Integer.toString(x);
 }
+    //System.out.print(binaryNo[j]);
+    return res;
+}
+
